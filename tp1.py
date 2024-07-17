254c89221792684b1dd7d853675a0df0d0f51c51
Tom Quez <tom.quez@live.fr>
2024-07-17 10:42:52 +0200

    third commit

--- tp1.py
+++ tp1.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine, text;
+from sqlalchemy.orm import Session;
 
 engine=create_engine('sqlite+pysqlite:///:memory:',echo=True)
 
@@ -24,3 +25,18 @@ with engine.connect() as conn:
     result2=conn.execute(text("SELECT x,y FROM some_table WHERE y > :y"),{"y":2})
     for row in result2:
         print(f"x: {row.x} y: {row.y}")
+    conn.execute(text("INSERT INTO some_table(x,y) VALUES (:x,:y)"),
+                 [{"x":11,"y":12},{"x":13,"y":14}])
+    conn.commit()
+
+stmt=text("SELECT x,y FROM some_table WHERE y>:y ORDER BY x,y")
+with Session(engine) as session:
+    result=session.execute(stmt,{"y":6})
+    for row in result:
+        print(f"x: {row.x} y: {row.y}")
+with Session(engine) as session:
+    result=session.execute(
+        text("UPDATE some_table SET y=:y WHERE x=:x"),
+        [{"x":9,"y":11},{"x":13,"y":15}]
+    )
+    session.commit()
